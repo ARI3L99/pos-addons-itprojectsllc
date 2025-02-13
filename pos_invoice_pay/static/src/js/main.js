cdb78ab05d9331e24a72bc7bf8961839d5cca33d
ARI3L <89934569+ARI3L99@users.noreply.github.com>
2025-02-13 09:26:36 -0300

    añado funcionalidad para validar facturas desde las work order

--- pos_invoice_pay/static/src/js/main.js
+++ pos_invoice_pay/static/src/js/main.js
@@ -255,6 +255,14 @@ models.PosModel = models.PosModel.extend({
             args: [id]
         });
     },
+    validate_invoice_pos: function (id) {
+        var result = $.Deferred();
+        return rpc.query({
+            model: 'account.invoice',
+            method: 'invoice_pos',
+            args: [id]
+        });
+    },
 
     get_invoices_to_render: function (invoices) {
         var self = this,
@@ -1127,6 +1135,9 @@ var InvoiceReceiptScreenWidget = screens.ReceiptScreenWidget.extend({
         this.$('.change-value').html(this.format_currency(order.invoice_to_pay.get_change()));
     },
     click_next: function() {
+        var invoice = this.pos.db.invoices[0].id
+        this.pos.validate_invoice_pos(invoice)
+
         this.gui.show_screen('products');
     },
 });
